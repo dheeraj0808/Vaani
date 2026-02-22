0292768de449cae6a9cd071a07f6d899ffaf4204
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-02-23 00:56:55 +0530

    change in mainlayout for the profile section

--- frontend/src/Components/layout/MainLayout.jsx
+++ frontend/src/Components/layout/MainLayout.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import Home from '../../pages/Home'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 const MainLayout = () => {
   return (
-    <>
-      <Home />
+    <div className="min-h-screen bg-black text-white">
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 ml-[280px] min-h-screen max-w-2xl border-r border-gray-800">
+          <Outlet />
+        </main>
+        {/* Right sidebar area - keeps content centered */}
+        <div className="hidden xl:block flex-1" />
+      </div>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default MainLayout
+export default MainLayout;
