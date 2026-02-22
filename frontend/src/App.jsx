a3189785e827ae7bd292cc3c87e30639d72c47ef
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-02-23 01:23:44 +0530

    changes for the side bar menu to redirect that pages in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import LandingPage from './pages/LandingPage';
 import MainLayout from './components/layout/MainLayout';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Explore from './pages/Explore';
+import Reels from './pages/Reels';
+import Messages from './pages/Messages';
+import Notifications from './pages/Notifications';
+import Threads from './pages/Threads';
+import Settings from './pages/Settings';
 
 function App() {
   return (
@@ -12,6 +18,12 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/reels" element={<Reels />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/threads" element={<Threads />} />
+          <Route path="/settings" element={<Settings />} />
         </Route>
       </Routes>
     </Router>
