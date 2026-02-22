5d8bca8370b34e1b4857e12eabce306896a12abc
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-02-23 00:44:59 +0530

    change in app.js for the profile view seciton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import MainLayout from './components/layout/MainLayout';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<MainLayout />} />
+        <Route element={<MainLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </Router>
   );
